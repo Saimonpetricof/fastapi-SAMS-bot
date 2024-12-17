6bcbc1521d5b5449b8093ef368bf08914df1ad57
Saimonpetricof <samyol41@gmail.com>
2024-12-17 17:56:03 +0700

    sda

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 app = FastAPI()
-
+token = '7617038367:AAE7aAYV0Ww1E1ZM7Ugggrbgs2BLXrHfmPU'
+link = 'https://api.telegram.org/bot' + token
 
 @app.get("/")
 async def root():
-    return {"message": " Работаем, как непонятно"}
+    return {"message": " Работаем, как - непонятно"}
 
-@app.post("/webhook")
-async def webhook():
-    return JSONResponse(status_code=200)
+@app.get("/webhook")
+async def webhook(request: Request):
+    update =request.get(link + "/sendMessage?chat_id=479761739&text=Привет_медвед")
+    return {"привет_медвед"}
